9bbdc11b3120f52aea9742d2bbaf2347f891f7f7
igor <iveretennikov77@gmail.com>
2023-04-04 21:29:16 +0300

    Добавьте файлы проекта.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Получение, создание редактирования заметок, валидации запросов 
+
+///GUID — статистически уникальный 128-битный идентификатор. Его главная особенность — уникальность,
+/// которая позволяет создавать расширяемые сервисы и приложения без опасения конфликтов, вызванных совпадением ///
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate{ get; set; }
+        public DateTime?  EditDate { get; set; }
+    }
+}
